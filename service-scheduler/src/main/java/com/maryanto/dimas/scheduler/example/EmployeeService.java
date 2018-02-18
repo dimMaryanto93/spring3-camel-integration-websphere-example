3144a85d232a8f2eef8b55416fe92e15f7f6d123
dimmaryanto93 <engineer.dimmaryanto93@outlook.com>
2018-02-18 18:08:41 +0700

    service quartz scheduler with database store delegate

--- service-scheduler/src/main/java/com/maryanto/dimas/scheduler/example/EmployeeService.java
+++ service-scheduler/src/main/java/com/maryanto/dimas/scheduler/example/EmployeeService.java
@@ -1,22 +1,12 @@
 package com.maryanto.dimas.scheduler.example;
 
-import org.apache.camel.Exchange;
-import org.apache.camel.Processor;
-import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Service;
 
 @Service
-public class EmployeeService extends RouteBuilder {
+public class EmployeeService {
 
-    @Override
-    public void configure() throws Exception {
-
-        from("quartz://myGroupName/myTimerName?cron=0/5+*+*+*+*+?")
-                .process(new Processor() {
-                    @Override
-                    public void process(Exchange exchange) throws Exception {
-                        System.out.println("I'm running every 5 sec...");
-                    }
-                });
+    public String messageInfo() {
+        return "message java version 1.8";
     }
+
 }
