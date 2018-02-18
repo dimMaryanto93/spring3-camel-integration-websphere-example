bd2449caffbf005086271d747d8d7ff259b66878
dimmaryanto93 <engineer.dimmaryanto93@outlook.com>
2018-02-18 17:43:31 +0700

    update project

--- service-scheduler/src/main/java/com/maryanto/dimas/scheduler/example/EmployeeService.java
+++ service-scheduler/src/main/java/com/maryanto/dimas/scheduler/example/EmployeeService.java
@@ -1,11 +1,22 @@
 package com.maryanto.dimas.scheduler.example;
 
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Service;
 
 @Service
-public class EmployeeService {
+public class EmployeeService extends RouteBuilder {
 
-    public String jumlahKaryawan() {
-        return "jumlahKaryawan ada 10";
+    @Override
+    public void configure() throws Exception {
+
+        from("quartz://myGroupName/myTimerName?cron=0/5+*+*+*+*+?")
+                .process(new Processor() {
+                    @Override
+                    public void process(Exchange exchange) throws Exception {
+                        System.out.println("I'm running every 5 sec...");
+                    }
+                });
     }
 }
